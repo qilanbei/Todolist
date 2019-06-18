e14f255cf3d86ae22241ce8d46c3f4d9f3a7ea01
niwei <niwei@phoenixtv.com>
2019-06-18 11:34:36 +0800

    edit about

--- src/about.js
+++ src/about.js
@@ -5,7 +5,7 @@ class About extends Component {
         return (
             <div style={{margin: '100px auto', textAlign: 'center'}}>
                 <h1>About me</h1>
-                <p>我是谁，我在哪，我在干嘛？？？？？？</p>
+                <p>我是谁，我在哪，我在干嘛 ？？？？？？</p>
             </div>
         )
 
